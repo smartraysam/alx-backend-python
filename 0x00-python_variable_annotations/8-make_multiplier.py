1a85b418503a81f1cc62bdef8b5cb2ca44db78e5
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:27:55 +0100

    pycodestyle formatting applied

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """Module that make multipliers"""
 from typing import Callable
 
 
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Function that returns a function that multiplies a float by multiplier"""
+    """Function that returns function that multiplies a float by multiplier"""
 
     def multiply_by_multiplier(number: float) -> float:
         """Function that multiplies a float by multiplier"""
