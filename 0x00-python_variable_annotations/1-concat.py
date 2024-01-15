ed07dd598f54e8b8102ca4142d4ed8a954c2077b
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:22:19 +0100

    update task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
-#!/usr/bim/env python3
+#!/usr/bin/env python3
 """Module that contains function to concatenate strings"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """Function that returns the sum of 2 floats"""
+    """Function that concatenates 2 strings"""
 
-    return str1 + str2 
+    return str1 + str2
