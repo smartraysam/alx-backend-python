e64c1e86f0b1e25d67cf9303fadac1af98d4608b
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:03:25 +0100

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains function to add floats"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns the sum of 2 floats"""
+
+    return str(n) 
