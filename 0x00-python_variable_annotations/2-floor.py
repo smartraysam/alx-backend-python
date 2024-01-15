35bd2c0d1f8f9d3098871774e83f5a264d194f4f
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:05:49 +0100

    Function to add floats

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains function to add floats"""
+import math
+
+
+def floor(n: float) -> int:
+    """Function that returns the sum of 2 floats"""
+
+    return math.floor(n)
