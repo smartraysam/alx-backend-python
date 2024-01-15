9ecdf751e6fb04ed9a48d93c30af8d3580dc7b22
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:30:54 +0100

    pycodestyle formatting applied

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,13 +3,14 @@
 from typing import Any, Union, Mapping, TypeVar
 
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
     """Function with unknown return types"""
     if key in dct:
         return dct[key]
     else:
         return default
-  
