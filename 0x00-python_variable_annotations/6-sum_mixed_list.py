506b1065d375cf88b5f18997feb9e46536f9fee7
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:11:33 +0100

    Taask 5, 6, 7

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that sum mixed list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function that returns the sum of a list of floats"""
+
+    return sum(mxd_lst)
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module the make a tuple out of a string and a float"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Function that returns a tuple"""
+    
+    return (k, v * v)
