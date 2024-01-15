dc5bc64ddf07453158c488650908f1dd6d4aa28e
Smart  Raysam <engsamtob@yahoo.com>
2024-01-16 00:12:55 +0100

    Module that make multipler

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+"""Module that make multipliers"""
+from typing import Callable
+
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Function that returns a function that multiplies a float by multiplier"""
+
+    def multiply_by_multiplier(number: float) -> float:
+        """Function that multiplies a float by multiplier"""
+
+        return number * multiplier
+
+    return multiply_by_multiplier
